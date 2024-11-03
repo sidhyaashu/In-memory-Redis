815b2da64df749670fed7f6740e32187528e08c8
sidhyaashu <ashutoshsidhya69@gmail.com>
2024-11-03 12:44:37 +0530

    practical

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import axios from "axios"
+import client from "./client.js"
+
+const app = express()
+
+
+
+app.get("/",async(req,res)=>{
+    const cacheValue = await client.get('todos')
+    if(cacheValue) return res.json(JSON.parse(cacheValue))
+
+    const {data} = await axios.get("https://jsonplaceholder.typicode.com/todos")
+
+    await client.set('todos',JSON.stringify(data))
+    await client.expire("todos",60)
+
+    return res.json(data)
+})
+
+
+app.listen(3000)
